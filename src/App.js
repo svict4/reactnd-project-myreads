5ce0298f92be4f83dae91e6c7d59770c3347c089
Simon <email@simonvictory.com>
2018-01-03 16:24:02 +1100

    removed comments, added API

--- src/App.js
+++ src/App.js
@@ -5,34 +5,15 @@ import { Link, Route } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
         <Route exact path='/search' render={() => (
           <div className="search-books">
             <div className="search-books-bar">
-              <Link className="close-search" to="/" onClick={() => this.setState({ showSearchPage: false })}>Close</Link>
+              <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input type="text" placeholder="Search by title or author"/>
-
               </div>
             </div>
             <div className="search-books-results">
@@ -47,7 +28,7 @@ class BooksApp extends React.Component {
             </div>
             <Bookshelf></Bookshelf>
             <div className="open-search">
-              <Link to="/search" onClick={() => this.setState({ showSearchPage: true })}>Add a book</Link>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
         )}/>
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import Book from './Book'
 
 class Bookshelf extends Component {
